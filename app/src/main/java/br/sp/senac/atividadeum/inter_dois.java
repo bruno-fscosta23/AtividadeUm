c574f34070d7e17889845067e81562df35af6d3e
bruno.fscosta <brunofsc_@hotmail.com>
2019-08-06 17:25:11 -0300

    atualização e criação Dois

---
+++ app/src/main/java/br/sp/senac/atividadeum/inter_dois.java
@@ -0,0 +1,14 @@
+package br.sp.senac.atividadeum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class inter_dois extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inter_dois);
+    }
+}
---
+++ app/src/main/java/br/sp/senac/atividadeum/projetoLogin_pag.java
@@ -0,0 +1,14 @@
+package br.sp.senac.atividadeum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class projetoLogin_pag extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.projeto_login_pag);
+    }
+}
